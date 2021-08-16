f00ae1eac5acdf2813abeaeda0481f3e744feaee
Warlord56x <danika200151@gmail.com>
2021-08-16 22:34:36 +0200

    version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # minimal_py_steam_wrapper
- A minimal steam wrapper made in python
+
+## A minimal steam wrapper made in python
+
+### Why was it made?
+
+- This little file was created when someone said that they wanted their discord bot (that was created in [Discord.py](https://github.com/Rapptz/discord.py)) to recommend games to them either periodically or with some command calls.
+
+- Then what happend was that there isn't a specific steam wrapper,
+  for this kind of work. It was either complex or not compatible etc...
+
+That covers the "Why was it made?" section.
+
+How does it work?
+
+- Basically it uses [requests](https://github.com/psf/requests) to do some GET requests on the "http://api.steampowered.com" api.
+
+- Stores the appids in a json file to avoid making unnecessarily calls on the api.
+
+- Using the appids from the file, it can constructs a url from the given appid (It's pretty simple).
+
+- There is a random recommend function, using random and the games.json file.
+
+# Links
+
+- [Requests](https://github.com/psf/requests)
+- [Discord.py](https://github.com/Rapptz/discord.py)
+- [The bot using this method](https://github.com/holorum/USSLongIslandBot)
---
+++ main.py
@@ -0,0 +1,59 @@
+# For this script to be functioning, it requires the "requests" module
+import requests as req
+import json
+import random
+
+steam = "http://api.steampowered.com"
+steam_store = "http://store.steampowered.com"
+gamesdict = {}
+
+# Updates the gamesdict
+def update_dict():
+    jupdater = open("games.json", "r")
+    gamesdict = json.load(jupdater)
+    return gamesdict
+gamesdict = update_dict()
+
+# Creates a steam store url based on appid and name of the game
+def create_store_url(appid, name):
+    """Creates a steam store url"""
+    newname = name.replace(" ","_")
+    return "{}/app/{}/{}".format(steam_store, appid, newname)
+
+
+# Some apps might be not games so it needs to be checked, also because the request returns a {'succes' : false} tuple
+def get_game(appid):
+    """Returns the game with the given 'appid', if it is a game, otherwise returns 'None'"""
+    game = "{}/api/appdetails?appids={}".format(steam_store,appid)
+    r = req.get(game).json()
+    fk = list(r.keys())[0]
+    try:
+        data = r[fk]["data"]
+        if data["type"] != "game":
+            raise KeyError
+        return data
+    except KeyError:
+        return None
+
+
+# Returns a random game 
+def get_random_game():
+    """Return a random game url"""
+    r = gamesdict
+    randn = random.randint(0, len(r["applist"]["apps"]))
+    game = get_game(r["applist"]["apps"][randn]["appid"])
+    while game == None:
+        randn = random.randint(0, len(r["applist"]["apps"]))
+        game = get_game(r["applist"]["apps"][randn]["appid"])
+    return game
+
+
+# Recommends a random steam game
+def recommend_game():
+    """Recommends a game returns the name and the store url"""
+    game = get_random_game()
+    recommend = str(
+        "{}\n"
+        "{}"
+    ).format(game["name"], create_store_url(game["steam_appid"], game["name"]))
+    return recommend
